9e9e3bd698393e0c5e5d5e3cee34ccf6f7cf3865
unknown <erifaisol93@gmail.com>
2020-05-26 13:26:52 +0700

    add gorm conn string and get all service

--- main.go
+++ main.go
@@ -3,19 +3,29 @@ package main
 import (
 	"net/http"
 
-	"github.com/gin-gonic/gin"
-
+	gorm "./Config"
 	S "./src/api/services"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
+
+	db := gorm.GetDB()
+
 	r := gin.Default()
+
+	r.Use(func(c *gin.Context) {
+		c.Set("db", db)
+		c.Next()
+	})
 
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"Message": "API running"})
 	})
 
 	r.POST("/auth", S.Login)
+
+	r.GET("/person", S.GetItem)
 
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
 }
---
+++ src/api/services/items.go
@@ -0,0 +1,22 @@
+package services
+
+import (
+	"net/http"
+
+	models "../models"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+)
+
+func GetItem(c *gin.Context) {
+
+	db := c.MustGet("db").(*gorm.DB)
+
+	var items []models.Person
+
+	db.Find(&items)
+
+	c.JSON(http.StatusOK, gin.H{"data": items})
+
+}
